d19f802f216baa6385d299a1d5af75b78f005929
leopoldo-1 <leopoldo.miranda18@tectijuana.edu.mx>
2022-06-12 23:55:43 -0700

    Settings updated

--- config/settings.py
+++ config/settings.py
@@ -131,3 +131,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
